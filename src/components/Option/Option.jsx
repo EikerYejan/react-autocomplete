4c1637816e8116f9b8bd01706f29602666b959fd
EikerYejan <eikeryejan@gmail.com>
2021-11-22 18:55:12 -0400

    feat(option): case insensitive highligtht

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -4,9 +4,20 @@ import './Option.css'
 class Option extends React.Component {
   render() {
     const { label, isSelected, searchTerm, ...restProps } = this.props
+    const content =
+      typeof searchTerm === `string`
+        ? label.replace(new RegExp(searchTerm, 'i'), `<span class="option-highlight" >${searchTerm}&nbsp;</span>`)
+        : label
 
     return (
-      <li {...restProps} title={label} className={`option ${isSelected && 'is--selected'}`} dangerouslySetInnerHTML={{__html:  label.replace(searchTerm, `<span class="option-highlight" >${searchTerm}&nbsp;</span>`)}} />  
+      <li
+        {...restProps}
+        title={label}
+        className={`option ${isSelected && 'is--selected'}`}
+        dangerouslySetInnerHTML={{
+          __html: content,
+        }}
+      />
     )
   }
 }
